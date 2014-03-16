8706c470a659e6d5fae41f8213739446f97ccfa7
Arjun Naik <arjun@arjunnaik.in>
2014-03-16 12:36:46 +0100

    Updated gradle build.

--- app/src/main/java/tu/dresden/studybloxx/utils/Constants.java
+++ app/src/main/java/tu/dresden/studybloxx/utils/Constants.java
@@ -3,7 +3,7 @@ package tu.dresden.studybloxx.utils;
 public class Constants
 {
 	// public static final String STUDYBLOXX_HOST = "http://192.168.1.16:8000";
-	public static final String STUDYBLOXX_HOST = "http://95.85.35.74:38722";
+	public static final String STUDYBLOXX_HOST = "http://188.226.151.191";
 	public static final String STUDYBLOXX_USERNAME = "tu.dresden.studybloxx.STUDYBLOXX_USERNAME";
 	public static final String STUDYBLOXX_PASSWORD = "tu.dresden.studybloxx.STUDYBLOXX_PASSWORD";
 	public static final String NOTE_UPLOAD_URL = STUDYBLOXX_HOST + "/bloxxdata/add-note/";
